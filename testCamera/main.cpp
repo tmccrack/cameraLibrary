503ad0cca933caffee4cb6b7d99d08a25f3d965d
Tyler <tylermmccracken@gmail.com>
2016-06-30 11:08:46 -0400

    Added socket test

--- testCamera/main.cpp
+++ testCamera/main.cpp
@@ -30,10 +30,10 @@ bool GetInput(long *buffer)
     printf("\n");
     printf("Acquire full images - 1\n");
     printf("Acquire sub frame image - 2\n");
-    printf("Retrieve current image buffer - 3\n");
-    printf("Abort acquistion - 4\n");
-    printf("Check if running - 5\n");
-    printf("Send data - 6\n");
+    printf("Acquire closed loop - 3\n");
+    printf("Retrieve current image buffer - 4\n");
+    printf("Abort acquistion - 5\n");
+    printf("Send data over socket - 6\n");
     printf("Shutdown camera - Any other key\n");
     std::cin >> i_input;
     printf("\n\n");
@@ -58,12 +58,12 @@ bool GetInput(long *buffer)
     }
 
     /*
-     * Sub frame
+     * Closed loop
      */
     else if(i_input == 3)
     {
         setFrameSizeLV(240, 272, 240, 273, 1, 1);
-        acquireClosedLoopLV(0.001);
+        acquireClosedLoopLV(0.000);
         return TRUE;
     }
 
@@ -90,6 +90,37 @@ bool GetInput(long *buffer)
         abortAcquisitionLV();
         return TRUE;
     }
+
+    /*
+     * Socket tests, toggle a bits
+     */
+    else if (i_input == 6)
+    {
+        int i = 0;
+        bool a = false;
+        SocketClient *sock = new SocketClient;
+        sock->openConnection();
+        while (i<10000)
+        {
+            if (a)
+            {
+                sock->sendData(5.0000, 5.0000);
+                a = false;
+            }
+            else
+            {
+                sock->sendData(0.0000, 0.0000);
+                a = true;
+            }
+            i++;
+        }
+        sock->sendData(0.0000, 0.0000);
+        sock->closeConnection();
+        delete sock;
+        return true;
+    }
+
+
 
     else    return FALSE;
 }
